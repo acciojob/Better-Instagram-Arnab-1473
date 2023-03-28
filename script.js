941b3591e33d36d4e19d6ba5661a4f800daeb1a8
Arnab-1473 <119725146+Arnab-1473@users.noreply.github.com>
2023-03-28 20:27:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+const draggableElements = document.querySelectorAll('.draggable');
+const container = document.getElementById('container');
+let dragStartIndex;
+
+draggableElements.forEach((element, index) => {
+  element.addEventListener('dragstart', () => {
+    dragStartIndex = index;
+  });
+
+  element.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  });
+
+  element.addEventListener('drop', () => {
+    const dragEndIndex = index;
+    swapElements(dragStartIndex, dragEndIndex);
+  });
+});
+
+function swapElements(fromIndex, toIndex) {
+  const fromElement = container.children[fromIndex];
+  const toElement = container.children[toIndex];
+  container.insertBefore(toElement, fromElement);
+}
